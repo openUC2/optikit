43fdfb3d131af11e8865af7764a7e2de3f2f12b1
Ethan Li <lietk12@gmail.com>
2026-07-03 16:20:11 -0700

    Reduce verbosity for defining primitive report unit tests

--- internal/optikit/report_test.go
+++ internal/optikit/report_test.go
@@ -7,110 +7,38 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/openUC2/optikit/exp/designs"
 )
 
-var reportPrimTests = []struct {
-	design  string
-	variant string
-}{
-	{
-		design: "primitives/cube-skeleton.dsn",
+var reports = map[string][]string{ // design -> variants
+	"primitives/cube-skeleton.dsn": {
+		"",
 	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "ZposXpos",
-	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "ZposYpos",
-	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "ZposXneg",
-	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "ZposYneg",
-	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "ZnegXpos",
-	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "ZnegYpos",
-	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "ZnegXneg",
-	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "ZnegYneg",
-	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "YposXpos",
-	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "YposZneg",
-	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "YposXneg",
-	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "YposZpos",
-	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "YnegXpos",
-	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "YnegZneg",
-	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "YnegXneg",
-	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "YnegZpos",
-	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "XposZneg",
-	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "XposYpos",
-	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "XposZpos",
-	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "XposYneg",
-	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "XnegZneg",
-	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "XnegYpos",
-	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "XnegZpos",
-	},
-	{
-		design:  "primitives/axes.dsn",
-		variant: "XnegYneg",
+	"primitives/axes.dsn": {
+		"ZposXpos",
+		"ZposYpos",
+		"ZposXneg",
+		"ZposYneg",
+		"ZnegXpos",
+		"ZnegYpos",
+		"ZnegXneg",
+		"ZnegYneg",
+		"YposXpos",
+		"YposZneg",
+		"YposXneg",
+		"YposZpos",
+		"YnegXpos",
+		"YnegZneg",
+		"YnegXneg",
+		"YnegZpos",
+		"XposZneg",
+		"XposYpos",
+		"XposZpos",
+		"XposYneg",
+		"XnegZneg",
+		"XnegYpos",
+		"XnegZpos",
+		"XnegYneg",
 	},
 }
 
@@ -121,41 +49,53 @@ func TestReportPrims(t *testing.T) {
 	}
 	examplesPath := path.Join(path.Dir(path.Dir(cwd)), "examples")
 
-	for _, test := range reportPrimTests {
-		name := fmt.Sprintf("%s:%s", test.design, test.variant)
-		t.Run(name, func(t *testing.T) {
-			dp := path.Join(examplesPath, "designs", test.design)
+	for design, variants := range reports {
+		for _, variant := range variants {
+			name := fmt.Sprintf("%s:%s", design, variant)
+			t.Run(name, func(t *testing.T) {
+				dp := path.Join(examplesPath, "designs", design)
 
-			t.Logf("load %s:%s", test.design, test.variant)
-			designDecl, err := LoadDesignDecl(dp, test.variant)
-			if err != nil {
-				t.Error(err)
-				return
-			}
-			var want, got []byte
+				t.Logf("load %s:%s", design, variant)
+				designDecl, err := LoadDesignDecl(dp, variant)
+				if err != nil {
+					t.Error(err)
+					return
+				}
 
-			for _, format := range []string{"json", "yaml"} {
-				reportName := "_primitives"
-				if test.variant != "" {
-					reportName += ":" + test.variant
+				for format := range fileExts {
+					checkPrimitives(t, variant, designDecl, dp, format)
 				}
-				t.Logf("report %s to %s", reportName, format)
-				reportName += "." + fileExts[format]
-				if got, err = ReportPrimitives(t.Context(), designDecl.Components, format); err != nil {
-					t.Error(err)
-				}
-				if want, err = os.ReadFile(path.Join(dp, reportName)); err != nil {
-					t.Error(err)
-				}
-				if !cmp.Equal(got, want) {
-					t.Errorf("diff (-want +got):\n%+v", cmp.Diff(want, got))
-				}
-			}
-		})
+			})
+		}
 	}
 }
 
 var fileExts = map[string]string{
 	"json": "json",
 	"yaml": "yml",
+}
+
+func checkPrimitives(
+	t *testing.T, variant string, designDecl designs.DesignDecl, dp, format string,
+) {
+	t.Helper()
+
+	reportName := "_primitives"
+	if variant != "" {
+		reportName += ":" + variant
+	}
+	t.Logf("report %s to %s", reportName, format)
+	reportName += "." + fileExts[format]
+
+	var want, got []byte
+	var err error
+	if got, err = ReportPrimitives(t.Context(), designDecl.Components, format); err != nil {
+		t.Error(err)
+	}
+	if want, err = os.ReadFile(path.Join(dp, reportName)); err != nil {
+		t.Error(err)
+	}
+	if !cmp.Equal(got, want) {
+		t.Errorf("diff (-want +got):\n%+v", cmp.Diff(want, got))
+	}
 }
