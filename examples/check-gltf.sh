bef65c1a0a7c63968bb56283bd4ef4d0a6154141
Ethan Li <lietk12@gmail.com>
2026-08-13 17:35:15 +0200

    Delete accidentally-commited test file

--- examples/check-gltf.sh
+++ examples/check-gltf.sh
@@ -4,8 +4,11 @@ script_dir="$(dirname "$(realpath "$BASH_SOURCE")")"
 tool_dir="$(dirname "$script_dir")/tools/gltf-checker"
 
 find "$script_dir" -type f | grep -E "\.gltf$|\.glb$" |
-  go tool rush -k -e \
-    "echo \"{}\"; npm --prefix=\"$tool_dir\" -s run start <\"{}\""
+  go tool rush -k \
+    "
+      echo \"{}\"
+      npm --prefix=\"$tool_dir\" -s run start <\"{}\"
+    "
 
 if [[ $? != 0 ]]; then
   echo "An asset failed validation; you can find error messages above by searching for [ERRO]"
