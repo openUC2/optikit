1977602e06e2d4591b10590922e6a0f0ddaf7990
Ethan Li <lietk12@gmail.com>
2026-08-19 11:51:39 +0200

    Decompose the diagonal mirror insert into its primitive components

---
+++ examples/designs/cube-mounted/mirror-diagonal.dsn/mounted-mirror.dsn/generate.go
@@ -0,0 +1,24 @@
+package example
+
+//go:generate -command comp go run ../../../../../main.go dev dsn comp
+//go:generate -command geom go run ../../../../../main.go dev dsn geom
+//go:generate -command mdl go run ../../../../../main.go dev mdl
+
+//go:generate pwd
+
+//go:generate mdl convert --output-format=glb "BUY - Mirror - 40x30x2.stp"
+//go:generate mdl convert --output-format=glb "BUY - Adhesive pad - 41x29x1.stp"
+//go:generate mdl convert --output-format=glb "PRT - 2022 - INSMIR45TH2.stp"
+
+//go:generate comp render-comps-g --format=dot _components-graph.dot
+//go:generate comp render-comps-g --format=svg _components-graph.svg
+
+//go:generate geom render-pos-g --format=dot _positions-graph.dot
+//go:generate geom render-pos-g --format=svg _positions-graph.svg
+//go:generate geom render-pos-p _positions-plot.html
+
+//go:generate geom report-prim --format=json _primitives.json
+//go:generate geom report-prim --format=yaml _primitives.yml
+
+//go:generate geom render-obj --format=gltf _objects.gltf
+//go:generate geom render-obj --format=glb _objects.glb
