796d04bf029a4fdb8447f6568ee3404aa8fd1e77
Ethan Li <lietk12@gmail.com>
2026-08-25 18:33:14 +0200

    Generate report as YAML before JSON to aid debugging any Infs & NaNs which break JSON

--- examples/designs/primitives/cube-skeleton.dsn/generate.go
+++ examples/designs/primitives/cube-skeleton.dsn/generate.go
@@ -17,8 +17,8 @@ package example
 //go:generate geom render-pos-g --format=svg _positions-graph.svg
 //go:generate geom render-pos-p _positions-plot.html
 
-//go:generate geom report-prim --format=json _primitives.json
 //go:generate geom report-prim --format=yaml _primitives.yml
+//go:generate geom report-prim --format=json _primitives.json
 
 //go:generate geom render-obj --format=gltf _objects.gltf
 //go:generate geom render-obj --format=glb _objects.glb
