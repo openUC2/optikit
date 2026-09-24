1f5ac3db793bbc621606f1b3da8b905610b9d15d
Ethan Li <lietk12@gmail.com>
2026-09-24 18:31:21 +0200

    Fix incorrect paths in `generate.go`

--- examples/designs/primitives/hikcamera-with-objective.dsn/mounted-objective.dsn/half-case.dsn/generate.go
+++ examples/designs/primitives/hikcamera-with-objective.dsn/mounted-objective.dsn/half-case.dsn/generate.go
@@ -1,8 +1,8 @@
 package example
 
-//go:generate -command comp go run ../../../../../main.go dev dsn comp
-//go:generate -command geom go run ../../../../../main.go dev dsn geom
-//go:generate -command mdl go run ../../../../../main.go dev mdl
+//go:generate -command comp go run ../../../../../../main.go dev dsn comp
+//go:generate -command geom go run ../../../../../../main.go dev dsn geom
+//go:generate -command mdl go run ../../../../../../main.go dev mdl
 
 //go:generate pwd
 
